21aeada387a3a5bca7eda16ab32b26c93c77302b
Spugnam <quentin.picard@gmail.com>
2016-10-23 15:13:13 -0400

    CP updates

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,43 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by quentin picard on 10/19/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol SwitchCellDelegate {
+    @objc optional func switchCell(switchCell: SwitchCell, didChangedValue value: Bool)
+    
+}
+
+class SwitchCell: UITableViewCell {
+
+    
+    @IBOutlet weak var switchLabel: UILabel!
+    
+    @IBOutlet weak var onSwitch: UISwitch!
+    
+    weak var delegate: SwitchCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        onSwitch.addTarget(self, action: #selector(SwitchCell.switchValueChanged), for: UIControlEvents.valueChanged)
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func switchValueChanged() {
+        print("Switch value Changed")
+        delegate?.switchCell?(switchCell: self, didChangedValue: onSwitch.isOn)
+    }
+}
